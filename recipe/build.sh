edd9910ef463027c332e3001ef7ad7e65bcc4638
Julien Schueller <schueller@phimeca.com>
2023-05-22 15:17:11 +0200

    Use rpath

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,10 @@ mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
   -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
   ..
 
 make install -j${CPU_COUNT}
-DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT}
+ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT}
